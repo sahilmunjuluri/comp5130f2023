10627cd679e8a39c4b69853cd80ba5880efed523
sahilmunjuluri <144755499+sahilmunjuluri@users.noreply.github.com>
2023-11-20 23:39:50 -0500

    Add files via upload

--- FrontendTravelApp/src/App.js
+++ FrontendTravelApp/src/App.js
@@ -1,1 +1,15 @@
+import {Route, Routes} from 'react-router-dom';
+import './App.css';
+import {Home, SingleHotel} from './pages'
 
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/hotels/:name/:address/:id/reserve" element = {<SingleHotel/>}/>
+    </Routes>
+    
+  );
+}
+
+export default App;
